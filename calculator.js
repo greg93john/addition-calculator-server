a44f18cb6524e5843582187fd84e3a63079669a8
Greg Johnson <greg93John@gmail.com>
2020-12-14 16:07:58 -0800

    Adds BMI calculator option to calculator
    adds an additional BMI calculator option to the server

--- calculator.js
+++ calculator.js
@@ -13,4 +13,15 @@ app.post('/', function (req, res){
   res.send("The sum of the two numbers is: " + result);
 });
 
+app.get('/bmicalculator', function(req, res) {
+  res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post('/bmicalculator', function(req, res) {
+  var weight = parseFloat(req.body.weight);
+  var height = parseFloat(req.body.height);
+  var result = weight/(height * height);
+  res.send("Your BMI is: " + result);
+});
+
 app.listen("3000", () => { console.log("Running Calculator app, on port: 3000"); });
